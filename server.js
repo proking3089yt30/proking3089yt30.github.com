31eed636d08879e6b7127b10dbaf8c990681037b
proking3089yt30 <150360282+proking3089yt30@users.noreply.github.com>
2023-11-14 14:09:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,12 @@
-// server.js
+// Get the elements from the document
+const weatherForm = document.getElementById("weather-form");
+const cityInput = document.getElementById("city-input");
+const weatherResult = document.getElementById("weather-result");
 
-const express = require('express');
-const request = require('request');
-const apiKey = '53984a0563fb3875b9b3a67fb22e5305'; // Replace with your actual API key
-
-const app = express();
-
-// Define routes and handle requests
-app.get('/', (req, res) => {
-  res.send('Welcome to the Proking3089yt30 GitHub Server');
-});
-
-app.get('/data', (req, res) => {
-  const url = `https://api.example.com/data?apiKey=${apiKey}`; // Replace with the actual API endpoint
-
-  // Make a request to the API using the apiKey
-  request(url, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      const data = JSON.parse(body);
-      res.json(data);
-    } else {
-      res.status(response.statusCode).send(error);
-    }
-  });
-});
-
-// Start the server
-const port = 3000; // Replace with your desired port number
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Define a function to fetch the weather data from an API
+function getWeatherData(city) {
+    // Use the fetch method to make a request to the API
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=YOUR_API_KEY`)
+    .then(response => response.json()) // Convert the response to JSON format
+    .then(data => { // Use the data to display the weather information
+        // Get the temperature
